1af617a05192334a908cb323399024e039cafb07
Joice Fogaça <joi.fogaca@gmail.com>
2024-04-11 15:49:05 -0300

    test: :white_check_mark:

--- src/app/heroes/services/heroes.service.spec.ts
+++ src/app/heroes/services/heroes.service.spec.ts
@@ -1,16 +1,29 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { HeroesService } from './heroes.service';
+import { HttpClient } from '@angular/common/http';
 
-describe('HeroesService', () => {
+fdescribe('HeroesService', () => {
   let service: HeroesService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(HeroesService);
+    http = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call the get method with the correct endpoint', () => {
+    const spy = spyOn(http, 'get').and.callThrough();
+    service.getHeroes('test');
+    expect(spy).toHaveBeenCalled();
   });
 });
